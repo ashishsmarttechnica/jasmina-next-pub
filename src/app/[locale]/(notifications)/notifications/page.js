d69de9221b8ae773467b330326004775f1c4ea9d
ashishsmarttechnica <ashish.smarttechnica@gmail.com>
2025-07-22 15:40:15 +0530

    check

--- src/app/[locale]/(notifications)/notifications/page.js
+++ src/app/[locale]/(notifications)/notifications/page.js
@@ -1,19 +1,15 @@
+import NotificationMainFeed from "@/components/notifications/NotificationMainFeed";
 import { getSeoMeta } from "@/lib/seoMetadata";
-import React from "react";
-import NotificationMainFeed from "../../../../components/notifications/NotificationMainFeed";
-// import ChatConnection from "../../../../components/chat/defaultChat/ChatConnection";
 
 export const metadata = getSeoMeta({
   title: "Feed | Jasmina",
   path: "/notification",
 });
 
-
 const page = () => {
   return (
     <div className="py-5">
-        <NotificationMainFeed/>
-      {/* <JobsMainPage /> */}
+      <NotificationMainFeed />
     </div>
   );
 };
